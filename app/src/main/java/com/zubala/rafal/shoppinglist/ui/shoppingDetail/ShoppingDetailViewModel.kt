1794d2dfe192efc9c992269a485c9aee74b5df0b
Rafał Zubala <rzubala@gmail.com>
2019-10-27 12:57:53 +0100

    added videModelScope support

--- app/src/main/java/com/zubala/rafal/shoppinglist/ui/shoppingDetail/ShoppingDetailViewModel.kt
+++ app/src/main/java/com/zubala/rafal/shoppinglist/ui/shoppingDetail/ShoppingDetailViewModel.kt
@@ -2,14 +2,16 @@ package com.zubala.rafal.shoppinglist.ui.shoppingDetail
 
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.zubala.rafal.shoppinglist.database.ShoppingDatabaseDao
 import com.zubala.rafal.shoppinglist.database.ShoppingDetail
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ShoppingDetailViewModel(shoppingDetailId: Long, dataSource: ShoppingDatabaseDao) : ViewModel() {
     val database = dataSource
-
-    private val viewModelJob = Job()
 
     private val shoppingDetail = MediatorLiveData<ShoppingDetail>()
 
@@ -17,10 +19,5 @@ class ShoppingDetailViewModel(shoppingDetailId: Long, dataSource: ShoppingDataba
 
     init {
         shoppingDetail.addSource(database.getShoppingDetailWithId(shoppingDetailId), shoppingDetail::setValue)
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        viewModelJob.cancel()
     }
 }
